f49b43ef3d321f271bd9e69859ba6083f3bba05f
henryandesjr <henryandesjr@gmail.com>
2023-08-19 00:44:35 +0800

    Added 3 new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NewcontactComponent } from './newcontact/newcontact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    HomeComponent,
+    ContactsComponent,
+    NewcontactComponent
   ],
   imports: [
     BrowserModule,
